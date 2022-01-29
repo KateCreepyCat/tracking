9c0065b186ff2be196b9b18edd61dc433363f59f
kate <kateryna.tomonova@gmail.com>
2022-01-29 19:06:42 +0200

    track

---
+++ database/migrations/2022_01_29_170243_create_tracks_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTracksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tracks', function (Blueprint $table) {
+            $table->id();
+            $table->string('track', 22);
+            $table->foreignId('customer_id');
+            $table->unsignedInteger('order_id');
+            $table->unsignedTinyInteger('stage');
+            $table->text('date_text');
+            $table->text('problem_text');
+            $table->boolean('update');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tracks');
+    }
+}
